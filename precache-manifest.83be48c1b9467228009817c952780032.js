ab9ef7f11784a035d95c04af8d4677dceadd7361
DESKTOP-VFTMQ9S\User <youngun45@naver.com>
2020-12-28 01:49:01 +0900

    Updates

---
+++ precache-manifest.83be48c1b9467228009817c952780032.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = [
+  {
+    "revision": "24a976a1b6435193063f",
+    "url": "/React-MyPortFolio/static/css/main.aae84c8e.chunk.css"
+  },
+  {
+    "revision": "24a976a1b6435193063f",
+    "url": "/React-MyPortFolio/static/js/main.24a976a1.chunk.js"
+  },
+  {
+    "revision": "731c2605077cb45a488f",
+    "url": "/React-MyPortFolio/static/js/1.731c2605.chunk.js"
+  },
+  {
+    "revision": "517ec99f72fc22b931a0",
+    "url": "/React-MyPortFolio/static/js/runtime~main.517ec99f.js"
+  },
+  {
+    "revision": "b53ff55e09a092f9f0cbbf74af01b715",
+    "url": "/React-MyPortFolio/static/media/github-logo.b53ff55e.svg"
+  },
+  {
+    "revision": "dc6182491d71086c7f9de7ce76a332c4",
+    "url": "/React-MyPortFolio/static/media/instagram.dc618249.png"
+  },
+  {
+    "revision": "cc791bec145b9d9a77a047629fe32fd7",
+    "url": "/React-MyPortFolio/static/media/600px-Python-logo-notext.svg.cc791bec.png"
+  },
+  {
+    "revision": "876a8325687358ce46ec26be29118535",
+    "url": "/React-MyPortFolio/static/media/react-1.876a8325.svg"
+  },
+  {
+    "revision": "11e8af26618daecab1b13cab7387ce65",
+    "url": "/React-MyPortFolio/static/media/900px-JavaScript-logo.11e8af26.png"
+  },
+  {
+    "revision": "58393424b2ab7ba7027f214660b11677",
+    "url": "/React-MyPortFolio/static/media/cpppp-490x490.58393424.png"
+  },
+  {
+    "revision": "faae11a6b87547339411b40ac228d4d7",
+    "url": "/React-MyPortFolio/static/media/css.faae11a6.png"
+  },
+  {
+    "revision": "8c54ec0727b7919b84cd52a2efc47814",
+    "url": "/React-MyPortFolio/static/media/github-logo.8c54ec07.png"
+  },
+  {
+    "revision": "7e8c18039b613c21c3e7b820282f14b5",
+    "url": "/React-MyPortFolio/static/media/HTML.7e8c1803.png"
+  },
+  {
+    "revision": "3b96c497d8497caad056c672117d44e6",
+    "url": "/React-MyPortFolio/static/media/icons8-c-programming-500.3b96c497.png"
+  },
+  {
+    "revision": "74e943d82452f81f6a0bb0b51ac6785d",
+    "url": "/React-MyPortFolio/static/media/java.74e943d8.png"
+  },
+  {
+    "revision": "2bb3e0b27794781885224b100099b27c",
+    "url": "/React-MyPortFolio/static/media/mysql.2bb3e0b2.png"
+  },
+  {
+    "revision": "7ed8a808420a41cc91afa6236e7a1dd4",
+    "url": "/React-MyPortFolio/static/media/Tux.7ed8a808.png"
+  },
+  {
+    "revision": "96f372af44370c18c5fd7b46f795ad9b",
+    "url": "/React-MyPortFolio/static/media/Windows-Logo.96f372af.png"
+  },
+  {
+    "revision": "969fcb169494b244c3d371ea7d4fbb59",
+    "url": "/React-MyPortFolio/static/media/Spring.969fcb16.png"
+  },
+  {
+    "revision": "fcc769e10058f5c5b705a024e638500f",
+    "url": "/React-MyPortFolio/static/media/OracleDatabase.fcc769e1.png"
+  },
+  {
+    "revision": "574e73f91f5fe24f4fce062885430c94",
+    "url": "/React-MyPortFolio/static/media/background.574e73f9.png"
+  },
+  {
+    "revision": "7d5e38dc56c730ce552a81dfe3bfcdf8",
+    "url": "/React-MyPortFolio/static/media/myprofle_3.7d5e38dc.jpg"
+  },
+  {
+    "revision": "209dd0503833c1a41bf74d7a58d244b0",
+    "url": "/React-MyPortFolio/static/media/virtual.209dd050.png"
+  },
+  {
+    "revision": "83bffd84d77f844c389f726a8f24d4ab",
+    "url": "/React-MyPortFolio/static/media/smartmirror.83bffd84.png"
+  },
+  {
+    "revision": "37e586fb57257d6664d563a2bf3875bf",
+    "url": "/React-MyPortFolio/static/media/768px-Kotlin-logo.svg.37e586fb.png"
+  },
+  {
+    "revision": "b0c12b3217284d8a3cfc7ca8f172b70d",
+    "url": "/React-MyPortFolio/static/media/firebase_logo.b0c12b32.png"
+  },
+  {
+    "revision": "23d4be78082407fbde75192f49f7f6b3",
+    "url": "/React-MyPortFolio/static/media/icons8-nodejs-500.23d4be78.png"
+  },
+  {
+    "revision": "daeaaf51189d49c32411056c83360f41",
+    "url": "/React-MyPortFolio/static/media/mongodb-logo-1.daeaaf51.png"
+  },
+  {
+    "revision": "59c467536111a981d904a050d335c2be",
+    "url": "/React-MyPortFolio/static/media/Redux.59c46753.png"
+  },
+  {
+    "revision": "c1313bb554b06efbd7418d73f79e4cd0",
+    "url": "/React-MyPortFolio/index.html"
+  }
+];
